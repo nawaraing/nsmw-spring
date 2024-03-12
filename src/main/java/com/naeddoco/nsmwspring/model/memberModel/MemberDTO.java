9e185a599ab4977f3d10077b8e5a1aa892b4be4d
Gitkonium <106023908+Gitkonium@users.noreply.github.com>
2024-03-12 16:31:14 +0900

    [feat] lombok을 이용한 get set 삭제

--- src/main/java/com/naeddoco/nsmwspring/model/memberModel/MemberDTO.java
+++ src/main/java/com/naeddoco/nsmwspring/model/memberModel/MemberDTO.java
@@ -1,5 +1,8 @@
 package com.naeddoco.nsmwspring.model.memberModel;
 
+import lombok.Data;
+
+@Data
 public class MemberDTO {
 	
 	private String memberID; // 아이디
@@ -12,76 +15,6 @@ public class MemberDTO {
 	private String authority;// 권한(admin or user)
 	private String memberState; // 상태(탈퇴 or 고스트 등등)
 	
-	public String getMemberID() {
-		return memberID;
-	}
 	
-	public void setMemberID(String memberID) {
-		this.memberID = memberID;
-	}
-	
-	public String getMemberPassword() {
-		return memberPassword;
-	}
-	
-	public void setMemberPassword(String memberPassword) {
-		this.memberPassword = memberPassword;
-	}
-	
-	public String getMemberName() {
-		return memberName;
-	}
-	
-	public void setMemberName(String memberName) {
-		this.memberName = memberName;
-	}
-	
-	public String getDayOfBirth() {
-		return dayOfBirth;
-	}
-	
-	public void setDayOfBirth(String dayOfBirth) {
-		this.dayOfBirth = dayOfBirth;
-	}
-	
-	public String getGender() {
-		return gender;
-	}
-	
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public String getAuthority() {
-		return authority;
-	}
-	
-	public void setAuthority(String authority) {
-		this.authority = authority;
-	}
-	
-	public String getMemberState() {
-		return memberState;
-	}
-	
-	public void setMemberState(String memberState) {
-		this.memberState = memberState;
-	}
 	
 }
