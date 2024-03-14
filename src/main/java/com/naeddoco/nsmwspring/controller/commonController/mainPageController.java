6116540480d6277fdf9f86ae82b830e8b3979d3b
Gitkonium <106023908+Gitkonium@users.noreply.github.com>
2024-03-14 17:25:14 +0900

    [feat] 데이터 흐름 확인용 컨트롤러

--- src/main/java/com/naeddoco/nsmwspring/controller/commonController/mainPageController.java
+++ src/main/java/com/naeddoco/nsmwspring/controller/commonController/mainPageController.java
@@ -1,18 +1,29 @@
 package com.naeddoco.nsmwspring.controller.commonController;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.naeddoco.nsmwspring.model.memberModel.MemberDTO;
+import com.naeddoco.nsmwspring.model.memberModel.MemberService;
 import com.naeddoco.nsmwspring.model.productModel.ProductDTO;
+import com.naeddoco.nsmwspring.model.productModel.ProductService;
 
 @Controller
 public class mainPageController {
 	
+	@Autowired
+	private MemberService memberService;
+	private ProductService productService;
+	
 	@GetMapping("/")
-	public String test(Model model, ProductDTO productDTO) {
-
-		return "main";
+	public String test(Model model, ProductDTO productDTO, MemberDTO memberDTO) {
+		
+		
+		
+		
+		return "index";
 			
 	}
 
