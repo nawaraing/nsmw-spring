cf0f5a7f663c5b2b78afa92c69ab9559450c6d53
Gitkonium <106023908+Gitkonium@users.noreply.github.com>
2024-03-18 16:12:29 +0900

    [feat] 상품 이미지 테이블 isert 추가

--- src/main/java/com/naeddoco/nsmwspring/model/productImageModel/ProductImageDTO.java
+++ src/main/java/com/naeddoco/nsmwspring/model/productImageModel/ProductImageDTO.java
@@ -1,5 +1,23 @@
 package com.naeddoco.nsmwspring.model.productImageModel;
 
+import lombok.Data;
+
+@Data
 public class ProductImageDTO {
+	
+	private int productImageID; // 인조 식별자
+	private int imageID; // 이미지 아이디
+	private int productID; // 상품 아이디
+	
+	@Override
+	public String toString() {
+		
+		return "ProductImageDTO ["
+			    + "productImageID = " + productImageID + ", "
+			    + "imageID = " + imageID + ", "
+				+ "productID = " + productID
+				+ "]";
+		
+	}
 
 }
