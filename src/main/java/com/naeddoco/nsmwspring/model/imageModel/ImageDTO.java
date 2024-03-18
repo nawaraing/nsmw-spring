52a5387e9af26d3bebe8f5da753764f779376f96
Jeonghui Ha <hajeonghui@gmail.com>
2024-03-18 14:33:19 +0900

    [feat] 이미지 DTO 주석 추가 및 searchCondition멤버변수 추가

--- src/main/java/com/naeddoco/nsmwspring/model/imageModel/ImageDTO.java
+++ src/main/java/com/naeddoco/nsmwspring/model/imageModel/ImageDTO.java
@@ -4,7 +4,9 @@ import lombok.Data;
 
 @Data
 public class ImageDTO {
-	private int imageID;
-	private String imagePath;
+	private int imageID; // 이미지아이디
+	private String imagePath; // 이미지 경로
 	
+    private String searchCondition; // 쿼리 분기
+
 }
