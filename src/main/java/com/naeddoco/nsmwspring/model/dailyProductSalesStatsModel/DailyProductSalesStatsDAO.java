d4fa548db934407aecd318b02d5e9cbddff2fdbe
하 정 희 <hajeonghui@gmail.com>
2024-03-21 17:43:17 +0900

    [feat] 상품별 판매 통계 조회(일자별) 모델 기능 구현

--- src/main/java/com/naeddoco/nsmwspring/model/dailyProductSalesStatsModel/DailyProductSalesStatsDAO.java
+++ src/main/java/com/naeddoco/nsmwspring/model/dailyProductSalesStatsModel/DailyProductSalesStatsDAO.java
@@ -19,19 +19,74 @@ public class DailyProductSalesStatsDAO {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
-	private static final String SELECTALL = "";
+
+	//관리자 페이지-상품별 판매 통계 조회(일자별)
+	//날짜와 시작행의 인덱스, 보여줄 상품 갯수를 입력받아 조회
+	//최대 검색일자 범위는 컨트롤러에서 확인 예정
+	private static final String SELECTALL_ADMIN_STAT_DATA = "SELECT "
+																+ "DPS.PRODUCT_ID, "
+																+ "P.PRODUCT_NAME ,"
+																+ "DPS.DAILY_TOTAL_CALCULATE_DATE, "
+																+ "DPS.DAILY_TOTAL_QUANTITY, "
+																+ "DPS.DAILY_TOTAL_GROSS_MARGINE, "
+																+ "DPS.DAILY_TOTAL_NET_PROFIT "
+															+ "FROM "
+																+ "DAILY_PRODUCT_SALES_STATS DPS "
+															+ "JOIN "
+																+ "PRODUCT P ON DPS.PRODUCT_ID = P.PRODUCT_ID "
+															+ "WHERE "
+																+ "DPS.DAILY_TOTAL_CALCULATE_DATE BETWEEN ? AND ? "
+																+ "LIMIT ?, ?";
+
+		
 
 	private static final String SELECTONE = "";
 	
-	private static final String INSERT = "";
+	//각 제품의 일별 판매 통계 - 일정 시간에 자동 추가
+	//추후 이벤트 트리거 등을 사용할 예정
+	private static final String INSERT = "INSERT INTO DAILY_PRODUCT_SALES_STATS ("
+											+ "PRODUCT_ID,"
+											+ "DAILY_TOTAL_CALCULATE_DATE, "
+											+ "DAILY_TOTAL_QUANTITY, "
+											+ "DAILY_TOTAL_GROSS_MARGINE,"
+											+ "DAILY_TOTAL_NET_PROFIT) "
+										+ "SELECT "
+											+ "O.PRODUCT_ID,"
+											+ "DATE(B.BUY_DATE) AS DAILY_TOTAL_CALCULATE_DATE, "
+											+ "SUM(O.BUY_QUANTITY) AS DAILY_TOTAL_QUANTITY, "
+											+ "SUM(O.PAYMENT_PRICE) AS DAILY_TOTAL_GROSS_MARGINE, "
+											+ "SUM(O.PAYMENT_PRICE) - (SUM(O.BUY_QUANTITY) * P.COST_PRICE) AS DAILY_TOTAL_NET_PROFIT "
+										+ "FROM ORDER_INFO O "
+										+ "JOIN BUY_INFO B ON O.BUY_INFO_ID = B.BUY_INFO_ID "
+										+ "JOIN PRODUCT P ON O.PRODUCT_ID = P.PRODUCT_ID "
+										+ "GROUP BY O.PRODUCT_ID, DATE(B.BUY_DATE)";
 	
 	private static final String UPDATE = "";
 
 	private static final String DELETE = "";
 	
+	
+	
 	public List<DailyProductSalesStatsDTO> selectAll(DailyProductSalesStatsDTO dailyProductSalesStatsDTO) {
-		log.debug("selectAll start");
-		return (List<DailyProductSalesStatsDTO>)jdbcTemplate.query(SELECTALL, new DailyProductSalesStatsRowMapper());
+
+		log.trace("selectAll 진입");
+		if(dailyProductSalesStatsDTO.getSearchCondition().equals("selectAdminStatProductDatas")) {
+
+			Object[] args = { dailyProductSalesStatsDTO.getAncStartDate(), dailyProductSalesStatsDTO.getAncEndDate(), dailyProductSalesStatsDTO.getAncStartRow(), dailyProductSalesStatsDTO.getAncSelectMax() };
+
+			log.trace("selectAdminStatProductDatas 진입");
+			try {
+				return (List<DailyProductSalesStatsDTO>)jdbcTemplate.query(SELECTALL_ADMIN_STAT_DATA,  args, new SelectAdminDailyProductSalesStatsRowMapper());
+			}
+			catch (Exception e) {
+
+				log.error("selectAdminStatProductDatas 예외/실패 ");
+
+				return null;
+			}
+		}
+		log.error("selectAll 실패");
+		return null;
 	}
 
 	
@@ -51,13 +106,13 @@ public class DailyProductSalesStatsDAO {
 	
 	public boolean insert(DailyProductSalesStatsDTO dailyProductSalesStatsDTO) {
 	
-//		int result = jdbcTemplate.update(INSERT);
-//		if(result <= 0) {
-//			log.debug("insert 실패");
+		int result = jdbcTemplate.update(INSERT);
+		if(result <= 0) {
+			log.debug("insert 실패");
 			return false;
-//		}
-//		log.debug("insert 성공");
-//		return true;
+		}
+		log.debug("insert 성공");
+		return true;
 	}
 
 	
@@ -92,18 +147,18 @@ class DailyProductSalesStatsRowMapper implements RowMapper<DailyProductSalesStat
 	public DailyProductSalesStatsDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
 		DailyProductSalesStatsDTO data = new DailyProductSalesStatsDTO();
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		
 		data.setDailyProductSalesStatsID(rs.getInt("DAILY_PRODUCT_SALES_STATS_ID"));
 		data.setProductID(rs.getInt("PRODUCT_ID"));
-		data.setDailyTotalCalculateDate(rs.getTimestamp("DAILY_TOTAL_CALCULATE_DATE"));
+		data.setDailyTotalCalculateDate(rs.getDate("DAILY_TOTAL_CALCULATE_DATE"));
 		data.setDailyTotalQuantity(rs.getInt("DAILY_TOTAL_QUANTITY"));
 		data.setDailyTotalGrossMargine(rs.getInt("DAILY_TOTAL_GROSS_MARGINE"));
 		data.setDailyTotalNetProfit(rs.getInt("DAILY_TOTAL_NET_PROFIT"));
 			
 		log.debug(Integer.toString(rs.getInt("DAILY_SALES_STATS_ID")));
 		log.debug(Integer.toString(rs.getInt("PRODUCT_ID")));
-		log.debug(sdf.format(rs.getTimestamp("DAILY_TOTAL_CALCULATE_DATE")));
+		log.debug(sdf.format(rs.getDate("DAILY_TOTAL_CALCULATE_DATE")));
 		log.debug(Integer.toString(rs.getInt("DAILY_TOTAL_QUANTITY")));
 		log.debug(Integer.toString(rs.getInt("DAILY_TOTAL_GROSS_MARGINE")));
 		log.debug(Integer.toString(rs.getInt("DAILY_TOTAL_NET_PROFIT")));
@@ -111,4 +166,30 @@ class DailyProductSalesStatsRowMapper implements RowMapper<DailyProductSalesStat
 		return data;
 	}
 	
+}
+
+@Slf4j
+class SelectAdminDailyProductSalesStatsRowMapper implements RowMapper<DailyProductSalesStatsDTO> {
+	@Override
+	public DailyProductSalesStatsDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+		DailyProductSalesStatsDTO data = new DailyProductSalesStatsDTO();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		data.setProductID(rs.getInt("DPS.PRODUCT_ID"));
+		data.setAncProductName(rs.getString("P.PRODUCT_NAME"));
+		data.setDailyTotalCalculateDate(rs.getDate("DPS.DAILY_TOTAL_CALCULATE_DATE"));
+		data.setDailyTotalQuantity(rs.getInt("DPS.DAILY_TOTAL_QUANTITY"));
+		data.setDailyTotalGrossMargine(rs.getInt("DPS.DAILY_TOTAL_GROSS_MARGINE"));
+		data.setDailyTotalNetProfit(rs.getInt("DPS.DAILY_TOTAL_NET_PROFIT"));
+			
+		log.debug("DPS.PRODUCT_ID : " + Integer.toString(rs.getInt("DPS.PRODUCT_ID")));
+		log.debug("PRODUCT_NAME : " + rs.getString("P.PRODUCT_NAME"));
+		log.debug("DAILY_TOTAL_CALCULATE_DATE : " + sdf.format(rs.getDate("DPS.DAILY_TOTAL_CALCULATE_DATE")));
+		log.debug("DAILY_TOTAL_QUANTITY : " + Integer.toString(rs.getInt("DPS.DAILY_TOTAL_QUANTITY")));
+		log.debug("DAILY_TOTAL_GROSS_MARGINE : " + Integer.toString(rs.getInt("DPS.DAILY_TOTAL_GROSS_MARGINE")));
+		log.debug("DAILY_TOTAL_NET_PROFIT : " + Integer.toString(rs.getInt("DPS.DAILY_TOTAL_NET_PROFIT")));
+		
+		return data;
+	}
 }
--- src/main/java/com/naeddoco/nsmwspring/model/dailyProductSalesStatsModel/DailyProductSalesStatsDTO.java
+++ src/main/java/com/naeddoco/nsmwspring/model/dailyProductSalesStatsModel/DailyProductSalesStatsDTO.java
@@ -1,17 +1,26 @@
 package com.naeddoco.nsmwspring.model.dailyProductSalesStatsModel;
 
+import java.sql.Date;
 import java.sql.Timestamp;
 
 import lombok.Data;
 
 @Data
 public class DailyProductSalesStatsDTO {
-	private int dailyProductSalesStatsID; // 일별 상품 통계 아이디
-	private int productID; // 상품 아이디
-	private Timestamp dailyTotalCalculateDate; // 통계일
-	private int dailyTotalQuantity; // 총 수량
-	private int dailyTotalGrossMargine; // 매출
-	private int dailyTotalNetProfit; //이익
+	private int dailyProductSalesStatsID; 		// 일별 상품 통계 아이디
+	private int productID;						// 상품 아이디
+	private Date dailyTotalCalculateDate; 		// 통계일
+	private int dailyTotalQuantity; 			// 총 수량
+	private int dailyTotalGrossMargine; 		// 매출
+	private int dailyTotalNetProfit; 			// 이익
 
-    private String searchCondition; // 쿼리 분기
+    private String searchCondition; 			// 쿼리 분기
+    
+    											// FK
+    private String ancProductName;				// 상품명
+    private Date ancStartDate;					// 검색 기간 시작일
+    private Date ancEndDate;					// 검색 기간 종료일
+    private int ancStartRow;					// 통계 시작 행 번호
+    private int ancSelectMax;					// 페이지별 출력할 row 수
+    
 }
