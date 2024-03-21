99839ed08758db520ba2f0ce1caee9a4c213888a
Gitkonium <106023908+Gitkonium@users.noreply.github.com>
2024-03-21 13:38:53 +0900

    [feat] 장바구니 페이징 컨트롤러 세션 없을 경우와 주석 추가

--- src/main/java/com/naeddoco/nsmwspring/controller/cart/EntryCartPageController.java
+++ src/main/java/com/naeddoco/nsmwspring/controller/cart/EntryCartPageController.java
@@ -15,24 +15,31 @@ import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class EntryCartPageController {
-	
+
 	@Autowired
 	private CartService cartService;
-	
+
 	@RequestMapping(value = "/entryCart", method = RequestMethod.GET)
 	public String entryCart(HttpSession session, Model model, CartDTO cartDTO) {
-		
+
 		String memberID = (String) session.getAttribute("memberID"); // 세션에 있는 유저 아이디 습득
+
+		if (memberID == null) { // 세션에 유저 아이디가 없을 시
+
+			return "redirect:/"; // 메인 페이지로 강제 이동
+
+		}
+
+		cartDTO.setSearchCondition("selectCartDatas"); // 검색 조건 설정
 		
-		cartDTO.setSearchCondition("selectCartDatas");
-		cartDTO.setMemberID(memberID);
-		
-		List<CartDTO> cartDTOList = cartService.selectAll(cartDTO);
-		
-		model.addAttribute("cartList", cartDTOList);
-		
-		return "user/cart";
-		
+		cartDTO.setMemberID(memberID); // 유저 아이디 DTO에 set
+
+		List<CartDTO> cartDTOList = cartService.selectAll(cartDTO); // selectAll 실행 후 반환 값 저장
+
+		model.addAttribute("cartList", cartDTOList); // 모델에 데이터 저장
+
+		return "forward:user/cart"; // 장바구니 페이지로 요청
+
 	}
 
 }
