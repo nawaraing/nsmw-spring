6f1fb4bc9a5c356378c449b1ca7125457be16e9a
JeongHoonSeok <154863581+JeongHoonSeok@users.noreply.github.com>
2024-03-21 14:48:43 +0900

    [feat] 관리자 대시보드 일자별 매출기능

--- src/main/java/com/naeddoco/nsmwspring/controller/dashBoard/DashboardPageController.java
+++ src/main/java/com/naeddoco/nsmwspring/controller/dashBoard/DashboardPageController.java
@@ -1,17 +1,34 @@
 package com.naeddoco.nsmwspring.controller.dashBoard;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.naeddoco.nsmwspring.model.dailySalesStatsModel.DailySalesStatsDTO;
+import com.naeddoco.nsmwspring.model.dailySalesStatsModel.DailySalesStatsService;
 
 @Controller
 public class DashboardPageController {
 	
+	@Autowired
+	private DailySalesStatsService dailySalesStatsService;
+	
 	@GetMapping("/dashboard")
-	public String dashboardPage() {
+	public String dashboardPage(DailySalesStatsDTO dailySalesStatsDTO, Model model) {
 		
 		System.out.println("[log] Controller dashboardPage요청");
 		
-		return "admin/dashboard";
+		dailySalesStatsDTO.setSearchCondition("selectAdminStatDateDatas");
 		
+		List<DailySalesStatsDTO> dailySalesStatsList = dailySalesStatsService.selectAll(dailySalesStatsDTO);
+		
+		dailySalesStatsList = dailySalesStatsService.selectAll(dailySalesStatsDTO);
+		
+		model.addAttribute("dashboardDailySalesStats", dailySalesStatsList);
+		
+		return "admin/dashboard";
 	}
 }
