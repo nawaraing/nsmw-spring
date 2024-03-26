ffc84916f8266b158cd26309c19c1c88aaf15a0d
Gitkonium <106023908+Gitkonium@users.noreply.github.com>
2024-03-26 17:23:56 +0900

    [docs] 주석 위치 변경

--- src/main/java/com/naeddoco/nsmwspring/model/productModel/ProductDTO.java
+++ src/main/java/com/naeddoco/nsmwspring/model/productModel/ProductDTO.java
@@ -5,24 +5,24 @@ import lombok.Data;
 @Data
 public class ProductDTO {
 	
-	private int productID; // 인조식별자 번호
-	private String productName; // 이름
-	private String productDetail; // 상세
-	private int costPrice; // 원가
-	private int retailPrice; // 소비자가
-	private int salePrice; // 판매가
-	private int stock; // 재고
-	private String ingredient; // 성분
-	private String dosage; // 용법
-	private String expirationDate; // 소비기한
-	private String registerDate; // 등록일
-	private String modifyDate; // 변경일 
-	private String saleState; // 판매 상태
+	private int productID;          // 인조식별자 번호
+	private String productName;     // 이름
+	private String productDetail;   // 상세
+	private int costPrice;          // 원가
+	private int retailPrice;        // 소비자가
+	private int salePrice;          // 판매가
+	private int stock;              // 재고
+	private String ingredient;      // 성분
+	private String dosage;          // 용법
+	private String expirationDate;  // 소비기한
+	private String registerDate;    // 등록일
+	private String modifyDate;      // 변경일 
+	private String saleState;       // 판매 상태
 	
 	private String searchCondition; // 쿼리 분기
 	
-	private String ancImagePath; // 이미지
-	private String ancCategory; // 카테고리
+	private String ancImagePath;    // 이미지
+	private String ancCategory;     // 카테고리
 	
 	@Override
 	public String toString() {
