eb6a7fa96c68cde55f3efac8dca24f6649c6f4de
하 정 희 <hajeonghui@gmail.com>
2024-04-03 14:11:35 +0900

    [feat] 사용자 다운로드 쿠폰 배포 중단 update 기능 구현

--- src/main/java/com/naeddoco/nsmwspring/model/couponModel/CouponDTO.java
+++ src/main/java/com/naeddoco/nsmwspring/model/couponModel/CouponDTO.java
@@ -17,36 +17,35 @@ public class CouponDTO {
     private String searchCondition; 			// 쿼리 분기 지정
     
     											// FK
+    private int ancCategoryID; 					// 카테고리 아이디
     private String ancCategoryName;     		// 쿠폰 카테고리 
-    private int ancDiscountAmount;      		// 원쿠폰 할인액
-    private int ancMinOrderAmount;      		// 원쿠폰 최소 구매 금액
-    private int ancDiscountRate;        		// 퍼센트쿠폰 할인율
-    private int ancMaxDiscountAmount;   		// 퍼센트쿠폰 최대 할인 금액
     
-	private String ancDeployCycle;				// 지급 주기
-	private String ancDeployBase;				// 지급 기준
-	
-	private int ancDiscount;					// 할인액 or 할인율 
-	private int ancAmount;						// 최소 구매 금액 or 최대 할인 금액
-	
-	private String ancGradeName;				// 등급 이름
-	
+    private int ancWonCouponID;					// 원화 쿠폰 타입 아이디
+    private int ancDiscountAmount;      		// 원화 쿠폰 할인액
+    private int ancMinOrderAmount;      		// 원화 쿠폰 최소 구매 금액
+    private int ancPercentageCouponID; 			// 퍼센트 쿠폰 타입 아이디
+    private int ancDiscountRate;        		// 퍼센트 쿠폰 할인율
+    private int ancMaxDiscountAmount;   		// 퍼센트 쿠폰 최대 할인 금액
+    private int ancDiscount;					// 할인액 or 할인율 
+    private int ancAmount;						// 최소 구매 금액 or 최대 할인 금액
+    private int ancCouponCategoryID; 			// 쿠폰 카테고리 아이디
+    
 	private String ancSearchKeyword;			// 검색 키워드
 	private String ancSortColumnName;			// 정렬 키워드
 
+	private int ancProvisionDownloadCouponID;	// 다운로드 쿠폰 아이디
 	private String ancDeployStatus;				// 배포 상태
 	private Timestamp ancDeployDeadline;		// 이미지 아이디
 	
-	private int ancProvisionDownloadCouponID;	// 쿠폰 다운로드 아이디
+	private int ancProvisionGradeCouponID;		// 등급 쿠폰 아이디
+	private String ancDeployCycle;				// 지급 주기
+	private String ancDeployBase;				// 지급 기준
+	private String ancGradeName;				// 등급 이름
+	
+	private int ancProvisionBatchCouponID;		// 쿠폰 전체 발송 아이디
+	
 	private int ancImageID;						// 이미지 아이디
 	private String ancImagePath; 				// 이미지 경로
-	
-	private int ancCouponCategoryID; 			// 쿠폰 카테고리 아이디
-	private int ancCategoryID; 					// 카테고리 아이디
-	private int ancPercentageCouponID; 			// 퍼센트 쿠폰 타입 아이디
-	private int ancWonCouponID;					// 원화 쿠폰 타입 아이디
-	
-	
 	
     @Override
    	public String toString() {
