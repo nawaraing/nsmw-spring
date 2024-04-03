e00cedeb614ed4a070a1393fcc1e6ebfb87066b9
강준현 <nawaddaing@gmail.com>
2024-04-03 11:29:10 +0900

    [fix] #44 statDate.jsp statProduct.jsp 날짜 선택 범위 유효성 검사

--- src/main/resources/static/admin/js/set-default-date.js
+++ src/main/resources/static/admin/js/set-default-date.js
@@ -13,14 +13,14 @@
     document.getElementById('end-month-input').value = todayMonth;
     document.getElementById('end-date-input').value = todayDate;
   
-    var Before30Days = new Date(today.getTime() - 30 * 24 * 60 * 60 * 1000);
+    var Before30Days = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 30);
     var Before30DaysY = Before30Days.getFullYear();
     var Before30DaysM = ('0' + (Before30Days.getMonth() + 1)).slice(-2); // 월은 0부터 시작하므로 +1을 해줍니다.
     var Before30DaysD = ('0' + Before30Days.getDate()).slice(-2); // 일자를 두 자리로 만들어줍니다.
     var startDate = Before30DaysY + '-' + Before30DaysM + '-' + Before30DaysD;
     document.getElementById('start-date-input').value = startDate;
 
-    var Before365Days = new Date(today.getTime() - 365 * 24 * 60 * 60 * 1000);
+    var Before365Days = new Date(today.getFullYear(), today.getMonth() - 11, today.getDate());
     var Before365DaysY = Before365Days.getFullYear();
     var Before365DaysM = ('0' + (Before365Days.getMonth() + 1)).slice(-2); // 월은 0부터 시작하므로 +1을 해줍니다.
     var startMonth = Before365DaysY + '-' + Before365DaysM;
