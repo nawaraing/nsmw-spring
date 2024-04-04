7b9789c6c62bcb45f6a0f01985bcb8fdb0731491
JeongHoonSeok <154863581+JeongHoonSeok@users.noreply.github.com>
2024-04-04 17:42:15 +0900

    [feat] [관] 다운로드 쿠폰 관리 페이지 카테고리 객체추가

--- src/main/java/com/naeddoco/nsmwspring/controller/couponDownload/EntryCouponDownloadPageController.java
+++ src/main/java/com/naeddoco/nsmwspring/controller/couponDownload/EntryCouponDownloadPageController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.naeddoco.nsmwspring.model.categoryModel.CategoryDTO;
+import com.naeddoco.nsmwspring.model.categoryModel.CategoryService;
 import com.naeddoco.nsmwspring.model.couponModel.CouponDTO;
 import com.naeddoco.nsmwspring.model.couponModel.CouponService;
 
@@ -16,9 +18,11 @@ public class EntryCouponDownloadPageController {
 	
 	@Autowired
 	private CouponService couponService;
+	@Autowired
+	private CategoryService categoryService;
 	
 	@RequestMapping(value = "/couponDownload", method = RequestMethod.GET)
-	public String entryCouponDownloadPage(CouponDTO couponDTO, Model model) {
+	public String entryCouponDownloadPage(CouponDTO couponDTO, CategoryDTO categoryDTO, Model model) {
 
 		couponDTO.setSearchCondition("selectAllDownloadCouponInfo");
 		
@@ -27,6 +31,10 @@ public class EntryCouponDownloadPageController {
 		System.out.println("[관] 다운로드 쿠폰 리스트 : " + downloadCouponList);
 
 		model.addAttribute("couponList", downloadCouponList);
+		
+		List<CategoryDTO> categoryList = categoryService.selectAll(categoryDTO);
+		
+		model.addAttribute("categoryList", categoryList);
 		
 		// 다운로드 쿠폰 관리 페이지 요청
 		return "admin/couponDownload";
